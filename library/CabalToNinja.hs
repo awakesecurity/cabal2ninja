50178dad0b9b1c7675f7f93340f10ba7fdcffeb7
Remy Goldschmidt <remygoldschmidt@awakenetworks.com>
2017-07-10 10:40:46 -0700

    Added source of cabal-preprocess

--- library/CabalToNinja.hs
+++ library/CabalToNinja.hs
@@ -20,7 +20,8 @@
 {-# OPTIONS_GHC #-}
 {-# OPTIONS_HADDOCK #-}
 
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 -- |
 --   Module      : CabalToNinja
@@ -34,31 +35,76 @@ module CabalToNinja
   ( module CabalToNinja -- FIXME: specific export list
   ) where
 
-import qualified Language.Ninja.AST     as AST
-import qualified Language.Ninja.Compile as Compile
-import qualified Language.Ninja.Errors  as Errors
-import qualified Language.Ninja.IR      as IR
-import qualified Language.Ninja.Lexer   as Lexer
-import qualified Language.Ninja.Misc    as Misc
-import qualified Language.Ninja.Mock    as Mock
-import qualified Language.Ninja.Parser  as Parser
-import qualified Language.Ninja.Pretty  as Pretty
-import qualified Language.Ninja.Shake   as Shake
+import qualified Language.Ninja.AST                 as AST
+import qualified Language.Ninja.Compile             as Compile
+import qualified Language.Ninja.Errors              as Errors
+import qualified Language.Ninja.IR                  as IR
+import qualified Language.Ninja.Lexer               as Lexer
+import qualified Language.Ninja.Misc                as Misc
+import qualified Language.Ninja.Mock                as Mock
+import qualified Language.Ninja.Parser              as Parser
+import qualified Language.Ninja.Pretty              as Pretty
+import qualified Language.Ninja.Shake               as Shake
 
-import           Control.Lens
+import qualified Distribution.Simple.Build          as Cabal
+import qualified Distribution.Simple.Configure      as Cabal
+import qualified Distribution.Simple.LocalBuildInfo as Cabal
+import qualified Distribution.Simple.PreProcess     as Cabal
+import qualified Distribution.Simple.Setup          as Cabal
+import qualified Distribution.Verbosity             as Cabal
 
-import           Data.Text              (Text)
-import qualified Data.Text              as Text
-import qualified Data.Text.IO           as Text
+import           Control.Lens.Getter
+import           Control.Lens.Iso
+import           Control.Lens.Lens
+import           Control.Lens.Setter
 
-import           System.Environment     (getArgs)
+import           Control.Monad
+
+import           Flow
+
+import           Data.Text                          (Text)
+import qualified Data.Text                          as Text
+import qualified Data.Text.IO                       as Text
+
+import           Filesystem.Path.CurrentOS          ((</>))
+import qualified Filesystem.Path.CurrentOS          as FP
+
+import           System.Environment                 (getArgs)
+
+--------------------------------------------------------------------------------
+
+-- cabalConfigure :: Misc.Path -> Cabal.ConfigFlags -> IO ()
+-- cabalConfigure projectPath configFlags = do
+--   let verbosity = C.normal
+--   Cabal.readPackageDescription verbosity
+--   let gpd = undefined
+--   let hbi = undefined
+--   lbi <- Cabal.configure (gpd, hbi) configFlags
+--   let dist = FP.encodeString ((projectPath ^. Misc.pathFP) </> "dist")
+--   Cabal.writePersistBuildConfig dist lbi
+
+cabalPreprocess :: Misc.Path -> IO ()
+cabalPreprocess projectPath = do
+  let verbosity = Cabal.normal
+  let dist = FP.encodeString ((projectPath ^. Misc.pathFP) </> "dist")
+  lbi <- Cabal.getPersistBuildConfig dist
+  let pkg = Cabal.localPkgDescr lbi
+  let components = Cabal.pkgEnabledComponents pkg
+  let handlers = Cabal.knownSuffixHandlers
+  Cabal.writeAutogenFiles verbosity pkg lbi
+  forM_ components
+    $ \c -> Cabal.preprocessComponent pkg c lbi False verbosity handlers
 
 generateNinja :: Misc.Path -> IO (AST.Ninja ())
-generateNinja projectDir = do
+generateNinja projectPath = do
   undefined -- FIXME
+
+--------------------------------------------------------------------------------
 
 tests :: IO ()
 tests = pure ()
+
+--------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
@@ -67,3 +113,5 @@ main = do
   ninja <- generateNinja (toPath input)
   let pretty = Pretty.prettyNinja ninja
   Text.writeFile output pretty
+
+--------------------------------------------------------------------------------
